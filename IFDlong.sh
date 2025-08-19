0c726e914bdeef3338628adb74e53d5a182cd164
jjlxJUn <jil700@pitt.edu>
2025-08-19 14:00:17 -0400

    Update IFDlong.sh

--- IFDlong.sh
+++ IFDlong.sh
@@ -248,54 +248,6 @@ check_tool "samtools" || exit 1
 check_tool "bedtools" || exit 1
 check_tool "minimap2" || exit 1
 
-#### check the reference genome and gene 
-# URLs for downloading genome and GTF (can be updated)
-declare -A GENOME_URL
-declare -A GTF_URL
-
-GENOME_URL[hg38]="http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz"
-GTF_URL[hg38]="http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/genes/hg38.gtf.gz"
-
-GENOME_URL[mm10]="http://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/mm10.fa.gz"
-GTF_URL[mm10]="http://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/genes/mm10.gtf.gz"
-
-FA=$(ls "$codeBase/refData/$ghc/"*.fa | head -n 1)
-GTF=$(ls "$codeBase/refData/$ghc/"*.gtf | head -n 1)
-
-# -------------------------
-# Download if missing
-# -------------------------
-if [ ! -f "$FA" ]; then
-    echo "Downloading $ghc FASTA..."
-    wget -O "$codeBase/refData/$ghc/genome.all.fa.gz" "${GENOME_URL[$ghc]}"
-    gunzip -f "$codeBase/refData/$ghc/genome.all.fa.gz"
-fi
-
-if [ ! -f "$GTF" ]; then
-    echo "Downloading $GENOME GTF..."
-    wget -O "$codeBase/refData/$ghc/genes.all.gtf.gz" "${GTF_URL[$ghc]}"
-    gunzip -f "$codeBase/refData/$ghc/genes.all.gtf.gz"
-fi
-
-
-#### Keep only main chromosomes
-echo "Filtering main chromosomes for $GENOME..."
-
-MAIN_FA="$codeBase/refData/$ghc/genome.fa"
-MAIN_GTF="$codeBase/refData/$ghc/genes.gtf"
-
-# Filter FASTA
-awk '/^>/ {p=($0 ~ /^>chr([1-9]|1[0-9]|2[0-2]|X|Y)$/)} p' $codeBase/refData/$ghc/genome.all.fa > "$MAIN_FA"
-rm $codeBase/refData/$ghc/genome.all.fa
-
-# Filter GTF
-awk '$1 ~ /^chr([1-9]|1[0-9]|2[0-2]|X|Y)$/ {print}' $codeBase/refData/$ghc/genes.all.gtf > "$MAIN_GTF"
-rm $codeBase/refData/$ghc/genes.all.gtf
-
-echo "Done! Main chromosomes saved:"
-echo "FASTA: $MAIN_FA"
-echo "GTF  : $MAIN_GTF"
-
 
 ### Main ###
 echo "Pipeline Begin $(date '+%Y-%m-%d %H:%M:%S')"
