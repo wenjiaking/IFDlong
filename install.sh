432ad874a19b1c786de0d76222cd7a7fb315061f
jjlxJUn <jil700@pitt.edu>
2025-08-19 16:59:19 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@
 chmod +x *.sh
 
 # Define install directory
-INSTALL_DIR="$(pwd)/tools/"
+INSTALL_DIR="$(pwd)/tools"
 mkdir -p "$INSTALL_DIR"
+
+
+TOOLS_PATH_FILE="./tools.path"
+> "$TOOLS_PATH_FILE" 
 
 
 check_and_install() {
     TOOL_NAME=$1
-    GITHUB_URL=$2
-    EXECUTABLE_NAME=$3
+    EXECUTABLE_NAME=$2
 
     echo "Checking for $TOOL_NAME"
 
@@ -31,20 +34,20 @@ check_and_install() {
    				tar -jxvf samtools-1.17.tar.bz2
    				cd samtools-1.17 || exit 1
                 ./configure --prefix="$INSTALL_DIR" && make && make install
-                export PATH="${INSTALL_DIR}/samtools-1.17:$PATH"
-                rm samtools-*.tar.bz2
+                echo "PATH=\"$INSTALL_DIR/samtools-1.17:\$PATH\"" >> "$TOOLS_PATH_FILE"
+                rm "$INSTALL_DIR/samtools-1.17.tar.bz2"
                 ;;
             minimap2)
                 git clone https://github.com/lh3/minimap2.git
                 cd minimap2 || exit 1
                 make
-                export PATH="${INSTALL_DIR}/minimap2:$PATH"
+                echo "PATH=\"$INSTALL_DIR/minimap2:\$PATH\"" >> "$TOOLS_PATH_FILE"
                 ;;
             bedtools)
                 git clone https://github.com/arq5x/bedtools2.git
                 cd bedtools2 || exit 1
                 make
-                export PATH="${INSTALL_DIR}/bedtools2/bin:$PATH"
+                echo "PATH=\"$INSTALL_DIR/bedtools2/bin:\$PATH\"" >> "$TOOLS_PATH_FILE"
                 ;;
         esac
 
@@ -52,39 +55,38 @@ check_and_install() {
 
         if command -v "$EXECUTABLE_NAME" >/dev/null 2>&1; then
             echo "$TOOL_NAME successfully installed at: $(command -v $EXECUTABLE_NAME)"
-            echo "$TOOL_NAME=\"$(command -v "$TOOL_NAME")\"" >> ./tools.path
+            echo "$EXECUTABLE_NAME=\"$(command -v "$EXECUTABLE_NAME")\"" >> "$TOOLS_PATH_FILE"
         else
             echo "Failed to install $TOOL_NAME"
         fi
     fi
 }
 
-# Run the check 
-check_and_install "samtools" "https://github.com/samtools/samtools" "samtools"
-check_and_install "minimap2" "https://github.com/lh3/minimap2" "minimap2"
-check_and_install "bedtools" "https://github.com/arq5x/bedtools2" "bedtools"
+# Run the checks
+check_and_install "samtools" "samtools"
+check_and_install "minimap2" "minimap2"
+check_and_install "bedtools" "bedtools"
 
 
-#Check if the version of gcc is >= 12.2.0
-gcc_version=`gcc -dumpversion`
-gcc_check=`echo -e "$gcc_version\n12.2.0" | sort -n | tail -n1`
-if [[ $gcc_chek = "12.2.0" ]] 
-then 
-   echo "Your version of gcc is $gcc_version."
-   echo "gcc must be >= 12.2.0 to install IFDlong. Exiting..."
-   exit 1
-fi
-
-echo "gcc check passed"
-
-R_path=`which Rscript 2>/dev/null`
-if [ -z $R_path ] ; then
-    echo "R not found!"
-    echo "Please go to http://www.r-project.org/ and follow the installation instructions."
-    echo "R version >= 4.4.0."
-    echo "Require the dependent packages: parallel, rlist, stringr, rtracklayer, Biostrings, dplyr, seqRFLP."
+### check gcc
+gcc_version=$(gcc -dumpversion)
+required_version="12.2.0"
+if [[ $(printf '%s\n%s' "$gcc_version" "$required_version" | sort -V | head -n1) != "$required_version" ]]; then
+    echo "Your version of gcc is $gcc_version."
+    echo "gcc must be >= $required_version to install IFDlong. Exiting..."
     exit 1
 fi
-echo "Rscript=\"$R_path\"" >> ./tools.path
+echo "gcc check passed"
+
+### check Rscript
+R_path=$(which Rscript 2>/dev/null)
+if [ -z "$R_path" ] ; then
+    echo "R not found!"
+    echo "Please install R version >= 4.4.0 and required packages: parallel, rlist, stringr, rtracklayer, Biostrings, dplyr."
+    exit 1
+fi
+echo "Rscript=\"$R_path\"" >> "$TOOLS_PATH_FILE"
+
+echo "Installation finished. Run 'source ./tools.path' to use installed tools."
 
 
