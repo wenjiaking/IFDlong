592f8e8b055d689ecc5e784640fdf4648f659281
jjlxJUn <jil700@pitt.edu>
2025-11-24 15:04:43 -0500

    Update Report.r

--- scripts/Report.r
+++ scripts/Report.r
@@ -14,7 +14,7 @@ suppressPackageStartupMessages({
     }
   }
   
-  install_and_load(c("Rcpp", "data.table", "parallel", "stringr", "rlist", "dplyr", "purr"))
+  install_and_load(c("Rcpp", "data.table", "parallel", "stringr", "rlist", "dplyr", "purrr"))
 })
 
 
